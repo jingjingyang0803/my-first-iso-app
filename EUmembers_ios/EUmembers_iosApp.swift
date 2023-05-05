c906b710765d04720cb2d587202a6a01439d0356
jingjing <jingdingdu@gmail.com>
2023-05-05 22:15:04 +0300

    Initial Commit

---
+++ EUmembers_ios/EUmembers_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  EUmembers_iosApp.swift
+//  EUmembers_ios
+//
+//  Created by jingjing yang on 5.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct EUmembers_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
